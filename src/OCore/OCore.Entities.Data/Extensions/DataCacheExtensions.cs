fbd83e1246f965de7297fc444e0a50749de43ba8
Lars Thomas Denstad <cocporn@gmail.com>
2020-01-10 21:10:26 +0100

    Rename method to perhaps make it possible to read cached data directly in the future (make ReadCached<T> return T instead of the proxy). Not sure how to do that now

--- src/OCore/OCore.Entities.Data/Extensions/DataCacheExtensions.cs
+++ src/OCore/OCore.Entities.Data/Extensions/DataCacheExtensions.cs
@@ -7,7 +7,7 @@ namespace OCore.Entities.Data.Extensions
 {
     public static class DataCacheExtensions
     {
-        public static async Task<DataCache<T>> ReadCached<T>(this IDataEntity<T> dataSource, TimeSpan? cacheFor)
+        public static async Task<DataCache<T>> GetDataCache<T>(this IDataEntity<T> dataSource, TimeSpan? cacheFor)
         {
             var dataCache = new DataCache<T>(dataSource);
             dataCache.CacheFor = cacheFor.Value;
