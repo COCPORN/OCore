6b6addb6c3b5a6d192c4b2d7a459239716ea12ff
Lars Thomas Denstad <cocporn@gmail.com>
2020-01-18 12:58:12 +0100

    Make it an implicit stream subscription attribute

--- src/OCore/OCore.Events/EventHandlerAttribute.cs
+++ src/OCore/OCore.Events/EventHandlerAttribute.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using Orleans;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OCore.Events
 {
-    public class EventHandlerAttribute : Attribute
+    public class EventHandlerAttribute : ImplicitStreamSubscriptionAttribute
     {
         public string EventName { get; private set; }
 
         public string Suffix { get; private set; }
 
-        public EventHandlerAttribute(string eventName, string suffix = null)
+        public EventHandlerAttribute(string eventName) : base(eventName)
+        {
+            EventName = eventName;
+        }
+
+        public EventHandlerAttribute(string eventName, string suffix) : base($"{eventName}:{suffix}")
         {
             EventName = eventName;
             Suffix = suffix;
