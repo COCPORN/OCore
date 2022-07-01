8009d5af5a5805ee51be46fa40b3f00b5c902d7f
Jeremy Carter <jeremychild@gmail.com>
2022-07-01 19:49:24 +1000

    Added serializer attributes for EntityState

--- src/OCore/OCore.Entities/EntityState.cs
+++ src/OCore/OCore.Entities/EntityState.cs
@@ -8,17 +8,24 @@ namespace OCore.Entities
     [GenerateSerializer]
     public class EntityState<T>
     {
+        [Id(0)]
         public bool Created { get; set; }
 
+        [Id(1)]
         public DateTimeOffset CreatedAt { get; set; }
+        [Id(2)]
         public DateTimeOffset UpdatedAt { get; set; }
 
+        [Id(3)]
         public int Version { get; set; }
 
+        [Id(4)]
         public Key Key { get; set; }
 
+        [Id(5)]
         public string TenantId { get; set; }
 
+        [Id(6)]
         public T Data;
     }
 }
